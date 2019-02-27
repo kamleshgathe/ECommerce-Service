f20f1c73193c269b784cc64bf7075b70ba128c6a
Imteyaz Khan <imteyaz.khan@jda.com>
2019-02-27 13:14:07 +0530

    DCT-7643 wrong object setting issue fix

--- src/main/java/com/jda/dct/chatservice/service/SituationRoomServiceImpl.java
+++ src/main/java/com/jda/dct/chatservice/service/SituationRoomServiceImpl.java
@@ -768,7 +768,7 @@ public class SituationRoomServiceImpl implements SituationRoomService {
 
     private ChatRoomResolution buildResolution(ResolveRoomDto request, String resolveBy) {
         ChatRoomResolution resolution = new ChatRoomResolution();
-        resolution.setResolution(resolution.getResolution());
+        resolution.setResolution(request.getResolution());
         resolution.setDate(new Date());
         resolution.setRemark(request.getRemark());
         resolution.setResolvedBy(resolveBy);
