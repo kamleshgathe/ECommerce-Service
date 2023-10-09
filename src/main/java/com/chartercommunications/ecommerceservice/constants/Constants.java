2e6b2536a25656081b27a3b75d4cccaed801ebaf
kamleshgathe <147280807+kamleshgathe@users.noreply.github.com>
2023-10-09 11:53:31 -0500

    Update Constants.java

--- src/main/java/com/chartercommunications/ecommerceservice/constants/Constants.java
+++ src/main/java/com/chartercommunications/ecommerceservice/constants/Constants.java
@@ -1,11 +1,3 @@
-/**
- * Copyright © 2023, JDA Software Group, Inc. ALL RIGHTS RESERVED.
- * <p>
- * This software is the confidential information of JDA Software, Inc., and is licensed
- * as restricted rights software. The use,reproduction, or disclosure of this software
- * is subject to restrictions set forth in your license agreement with JDA.
- */
-
 
 package com.chartercommunications.ecommerceservice.constants;
 
