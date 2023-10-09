6eb392e92b9a56fc057f7d70af484f6cb9639c34
kamleshgathe <147280807+kamleshgathe@users.noreply.github.com>
2023-10-09 11:50:41 -0500

    Update EcommerceServiceApplication.java

--- src/main/java/com/chartercommunications/ecommerceservice/EcommerceServiceApplication.java
+++ src/main/java/com/chartercommunications/ecommerceservice/EcommerceServiceApplication.java
@@ -1,10 +1,4 @@
-/**
- * Copyright © 2023, JDA Software Group, Inc. ALL RIGHTS RESERVED.
- * <p>
- * This software is the confidential information of JDA Software, Inc., and is licensed
- * as restricted rights software. The use,reproduction, or disclosure of this software
- * is subject to restrictions set forth in your license agreement with JDA.
- */
+
 package com.chartercommunications.ecommerceservice;
 
 import org.springframework.boot.SpringApplication;
